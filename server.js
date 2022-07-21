c960ae8daa4f87e6a1eb17124bfd8472bef62e1b
roman <roman@mac-pro.local>
2022-07-22 02:16:23 +0600

    first express/nodejs practice boot camp

---
+++ server.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const express = require('express');
+
+const HOST = '0.0.0.0';
+const PORT = 8080;
+
+const app = express();
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(PORT, HOST);
+console.log('Running on http://${HOST}:${PORT}');
